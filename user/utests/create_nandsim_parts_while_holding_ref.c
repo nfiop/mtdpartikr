b19ab248634a787b7e0edb642e4e1fa9cb24035e
Liav A. <liavalb@gmail.com>
2026-07-31 21:55:56 +0300

    Add the utests of create_nandsim_parts and its holding-ref variant
    These utests are checking attempts to add partitions, from an mtdpartctl
    context path.
    
    The partitions are valid in their parameters and the holding-ref variant
    ensures that if someone uses the proxy MTD device, then it cannot be
    re-partitioned until it is not used anymore.

---
+++ user/utests/create_nandsim_parts_while_holding_ref.c
@@ -0,0 +1,75 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0-only
+ * Copyright (c) 2026 Liav A
+ */
+
+#include "utest_helper.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/sysinfo.h>
+
+#define TEST_NAME "create_nandsim_parts_while_holding_ref"
+
+int main(int argc, char *argv[])
+{
+	int fd;
+	int ret;
+	int errno_ret;
+	int mtd_fd;
+	struct mtdpartctl_info mtd_info;
+	struct ext_mtd_partition_info part_info;
+	struct sysinfo info;
+	char mtd_path[sizeof("/dev/mtdXXXX")];
+
+	ret = sysinfo(&info);
+	errno_ret = ret < 0 ? -errno : 0;
+	SUBTEST_FAIL_IF_ERRNO_SET(TEST_NAME, "sysinfo", errno_ret);
+
+	SET_FD_WITH_MTDPARTCTL_DEVICE_OR_FAIL(TEST_NAME, fd, argc, argv);
+
+	{
+		ret = mtdpartctl_get_info(fd, &mtd_info);
+		SUBTEST_FAIL_IF_ERRNO_SET(
+		    TEST_NAME, "mtdpartctl_get_info", ret);
+	}
+
+	/* Sanity check - erase_sector_size > 1 */
+	{
+		ret = (mtd_info.erase_sector_size > 1) ? 0 : -EINVAL;
+		SUBTEST_FAIL_IF_ERRNO_SET(
+		    TEST_NAME, "verify_sane_erase_sector_size", ret);
+	}
+
+	/* Get a ref on the proxy MTD */
+	{
+		ret = snprintf(mtd_path, sizeof(mtd_path), "/dev/mtd%u",
+		    mtd_info.proxy_mtd_index);
+		ret = ret < 0 ? -EINVAL : 0;
+		SUBTEST_FAIL_IF_ERRNO_SET(TEST_NAME, "snprintf", ret);
+
+		mtd_fd = open(mtd_path, O_RDONLY);
+		if (mtd_fd < 0) {
+			TEST_FAIL_IF_REACHED(TEST_NAME, "open proxy mtd");
+		}
+	}
+
+	memset(&part_info, 0, sizeof(struct ext_mtd_partition_info));
+
+	/* Create 20 partitions with size of an erase block each */
+	for (int i = 0; i < 20; i++) {
+		ret = snprintf(part_info.base.name, sizeof(part_info.base.name),
+		    "Test partition %d", i);
+		errno_ret = ret < 0 ? -EINVAL : 0;
+		SUBTEST_FAIL_IF_ERRNO_SET(TEST_NAME, "snprintf", errno_ret);
+
+		part_info.base.offset = i * mtd_info.erase_sector_size;
+		part_info.base.length = mtd_info.erase_sector_size;
+		INVOKE_IOCTL_WITH_VARIABLE_AS_ERRNO(
+		    fd, ret, MTDPARTCTL_IOC_ADD_CONTEXT_PART, &part_info);
+		SUBTEST_FAIL_IF_ERRNO_SET(
+		    TEST_NAME, "adding-partition-via-context", ret);
+	}
+
+	ret = mtdpartctl_context_create_partitions(fd);
+	TEST_PASS_IF_ERRNO_NON_ZERO_AND_EXPECTED(TEST_NAME, ret, -EBUSY);
+}
